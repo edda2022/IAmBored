b1e8f79f5a94e1a09d66e00ce564f862900c4de5
edda2022 <candyreinecke@googlemail.com>
2022-11-29 08:33:14 +0100

    changes in JS Code in arrow functions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const what = ["Think about", "Create", "Read", "Listen to", "Call"]
-const topic = ["a newspaper", "yourself"]
+const topic = ["a newspaper", "yourself", "your bff"]
 const where = ["in your flat", "in the woods", "in the park", "outside", "in the kitchen"]
 
 // const randomWhat = what[Math.floor(Math.random() * what.length)];
@@ -17,19 +17,34 @@ const where = ["in your flat", "in the woods", "in the park", "outside", "in the
 // const task = randomWhat + ' ' + randomTopic + ' ' + randomWhere
 // console.log(task)
 
-const taskB = () => {
+// const taskA = () => {
+//     const randomWhat = what[Math.floor(Math.random() * what.length)]; 
+//     const randomTopic = topic[Math.floor(Math.random() * topic.length)];
+//     const randomWhere = where[Math.floor(Math.random() * where.length)];
+//     return randomWhat + ' ' + randomTopic + ' ' + randomWhere
+// }
+// console.log(taskA())
 
+//taskB => everything in once:
+
+
+const taskB = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+};
+const taskBMessage = () => {
+    return `${taskB(what)} ${taskB(topic)} ${taskB(where)}`
 }
 
-const taskA = () => {
-    const randomWhat = what[Math.floor(Math.random() * what.length)]; 
-    const randomTopic = topic[Math.floor(Math.random() * topic.length)];
-    const randomWhere = where[Math.floor(Math.random() * where.length)];
-    return randomWhat + ' ' + randomTopic + ' ' + randomWhere
-}
-console.log(taskA())
+console.log(taskBMessage());
+
+
+const returnOutput = () => {
+        document.getElementById("output").innerHTML = taskBMessage();
+} // ==> function expression MUST be set before it's called, otherwise DOM Manipulation will not work
 
 document.getElementById("outputbutton").addEventListener("click", returnOutput)
-function returnOutput () {
-    document.getElementById("output").innerHTML = taskA();
-}
+
+// function returnOutput () {
+//     document.getElementById("output").innerHTML = taskBMessage();
+//  } ==> Function Declaration: function is available everywhere -> doesn't matter where to put the function
+
